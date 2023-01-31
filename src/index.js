6bb818457821652a43c444535afc7ba5ab4eb48d
Leonardo Furtado <leonardomf1996@gmail.com>
2023-01-30 21:17:03 -0300

    feat: completed challenge

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import http from 'node:http';
+
+import { json } from './middlewares/json.js'
+import { ExtractQueryParams } from './helpers/extract-query-params.js';
+import { routes } from './routes.js';
+
+
+const server = http.createServer(async (request, response) => {
+   const { method, url } = request;
+
+   await json(request, response);
+
+   const route = routes.find(
+      route => route.method === method
+         && route.path.test(url)
+   );
+
+   if (route) {
+      const routeParams = request.url.match(route.path);
+
+      const { query, ...params } = routeParams.groups;
+
+      request.params = { ...params };
+      request.query = query ? ExtractQueryParams(query) : {};
+
+      return route.handler(request, response);
+   }
+
+   return response.writeHead(404).end();
+});
+
+server.listen(3333, () => {
+   console.log('Server running on port 3333');
+});
---
+++ src/routes.js
@@ -0,0 +1,81 @@
+import { randomUUID } from 'node:crypto';
+
+import { BuildRoutePath } from './helpers/build-route-path.js';
+import { Db } from './db.js';
+
+const db = new Db();
+
+export const routes = [   
+   {
+      method: 'POST',
+      path: BuildRoutePath('/tasks'),
+      handler: (request, response) => {
+         const requiredFields = ['title', 'description'];
+
+         for (const field of requiredFields) {
+            if (!request.body[field]) {
+               throw new Error(`Invalid or nonexistent field: ${field}`);
+            }
+         }
+            
+         const { title, description } = request.body; 
+
+         // Eu colocaria a data atual tbm em updated_at, mas na descrição não informa se precisa
+         const task = {
+            id: randomUUID(),
+            title,
+            description,
+            completed_at: null, 
+            created_at: new Date(),
+            updated_at: null 
+         }
+
+         db.create('tasks', task);
+
+         return response.writeHead(201).end(JSON.stringify(task));
+      }, 
+   },
+   {
+      method: 'GET',
+      path: BuildRoutePath('/tasks'),
+      handler: (request, response) => {         
+         const tasks = db.list('tasks');
+
+         return response.end(JSON.stringify(tasks));
+      },
+   },
+   {
+      method: 'PUT',
+      path: BuildRoutePath('/tasks/:id'),
+      handler: (request, response) => {     
+         const { id } = request.params;
+         const { title, description } = request.body;
+
+         db.update('tasks', id, { title, description });
+
+         return response.writeHead(204).end();
+      },
+   },
+   {
+      method: 'DELETE',
+      path: BuildRoutePath('/tasks/:id'),
+      handler: (request, response) => {     
+         const { id } = request.params;
+
+         db.delete('tasks', id);
+
+         return response.writeHead(204).end();
+      },
+   },
+   {
+      method: 'PATCH',
+      path: BuildRoutePath('/tasks/:id/complete'),
+      handler: (request, response) => {     
+         const { id } = request.params;
+
+         db.completeTask('tasks', id);
+
+         return response.writeHead(204).end();
+      },
+   },
+]
